8f20fdda25239b87880bb7a680124116d3a6d51a
Filipe Breda <filipe@tem.energy>
2026-06-07 17:13:30 +0100

    make it boot

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,26 @@
 #![no_std]
 #![no_main]
 
+use core::arch::asm;
+
 #[unsafe(no_mangle)]
-fn main() {}
+fn main() {
+    let msg = b"Hello, World!";
+    for &c in msg {
+        unsafe {
+            asm!(
+                "mov ah, 0x0E",
+                "mov al, {0}",
+                "int 0x10",
+                in(reg_byte) c,
+                out("ax") _,
+            );
+        }
+    }
+    unsafe {
+        asm!("hlt");
+    }
+}
 
 #[panic_handler]
 pub fn panic_handler(_info: &core::panic::PanicInfo) -> ! {
